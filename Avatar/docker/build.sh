7ec4e85fde3f49864397dd33e5d4af6f32c517ef
RendongZhang <rendong.zhang@vanderbilt.edu>
2025-02-24 09:56:19 -0600

    work env

--- Avatar/docker/build.sh
+++ Avatar/docker/build.sh
@@ -3,4 +3,4 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${CURRENT_DIR}/source.sh
 
-docker build -t $NAME -f ${CURRENT_DIR}/Dockerfile ${CURRENT_DIR}/..
+docker build  -t $NAME -f ${CURRENT_DIR}/Dockerfile ${CURRENT_DIR}/..
--- Avatar/docker/source.sh
+++ Avatar/docker/source.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-GSAC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../.."
-
 PORT=8087
 
-PARAMS="--net=host --ipc=host -u $(id -u ):$(id -g)"
+PARAMS="-p ${PORT}:${PORT} --net=host --ipc=host -u $(id -u ${USER}):$(id -g ${USER})"
 NAME="gsac"
-# VOLUMES="-v /${GSAC_DIR}:/gsac -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro"
-VOLUMES="-v /${GSAC_DIR}:/gsac"
+VOLUMES="-v /:/mounted"
