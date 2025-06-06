a02d0666aed3b47ab4347d0b6d2943d93652e4cc
Fahad Maqsood Qazi <50149692+fahadmaqsood@users.noreply.github.com>
2025-06-06 16:23:10 +0500

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -11,22 +11,26 @@ pip install git+https://github.com/facebookresearch/pytorch3d.git
 
 cd ${GSAC_DIR}/Preprocessor
 
+mkdir -p tools/
+mkdir -p common/utils/
+
 if [ ! -d "common/utils" ]; then
     echo "Error: /common/utils directory does not exist."
     exit 1
 fi
 
 if [ ! -d "tools/" ]; then
+    
     echo "Downloading TOOLS ."
     wget -O /tmp/human_model_files.tar \
     "https://huggingface.co/RendongZhang/GSAC-Dependencies/resolve/main/human_model_files.tar"
     tar -xf /tmp/human_model_files.tar -C common/utils
     rm /tmp/human_model_files.tar
 
-    wget -O /tmp/tools.tar \
+    wget -O /tmp/tools.zip \
         "https://huggingface.co/RendongZhang/GSAC-Dependencies/resolve/main/tools.tar"
-    tar -xf /tmp/tools.tar -C .
-    rm /tmp/tools.tar
+    unzip /tmp/tools.zip -d .
+    rm /tmp/tools.zip
 
     wget -O /tmp/sapiens.tar \
         "https://huggingface.co/RendongZhang/GSAC-Dependencies/resolve/main/sapiens.tar"
